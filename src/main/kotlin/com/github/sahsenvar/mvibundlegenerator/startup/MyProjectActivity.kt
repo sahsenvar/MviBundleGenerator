733b07dc0616814bb7d55c8181bc246934642836
GitHub Action <action@github.com>
2025-07-21 16:31:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/sahsenvar/mvibundlegenerator/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/sahsenvar/mvibundlegenerator/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.sahsenvar.mvibundlegenerator.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
